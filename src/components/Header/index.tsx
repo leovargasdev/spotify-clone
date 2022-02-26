8737f1e17e982b8dc1f14004384cc256bca9a2a8
Leonardo Vargas <leu1607@hotmail.com>
2022-02-26 17:49:54 -0300

    feat: finalizando o header da aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,38 +1,58 @@
+import Link from 'next/link'
+import { useState } from 'react'
+import { RiExternalLinkFill } from 'react-icons/ri'
 import { FaChevronLeft, FaChevronRight, FaCaretDown } from 'react-icons/fa'
 
 import styles from './styles.module.scss'
 
-export const Header = () => (
-  <header className={styles.container} id="main-header">
-    <div className={styles.controll}>
-      <button type="button" onClick={() => console.log('click')}>
-        <FaChevronLeft />
-      </button>
-      <button type="button" onClick={() => console.log('click')}>
-        <FaChevronRight />
-      </button>
-    </div>
-    <div className={styles.profile}>
-      <div
-        className={styles['profile--info']}
-        onClick={() => console.log('open menu')}
-      >
-        <div className={styles['profile--info__image']}>
-          <img
-            src="https://avatars.githubusercontent.com/u/11177716?v=4"
-            alt=""
-          />
-        </div>
-        <strong>Leonardo Vargas</strong>
+const NAVIGATION = [
+  { name: 'Conta', pathname: '/', icon: <RiExternalLinkFill /> },
+  { name: 'Perfil', pathname: '/', icon: '' },
+  { name: 'Sair', pathname: '/', icon: '' }
+]
 
-        <FaCaretDown />
+export const Header = () => {
+  const [isActive, setIsActive] = useState<boolean>(false)
+
+  return (
+    <header className={styles.container} id="main-header">
+      <div className={styles.controll}>
+        <button type="button" onClick={() => console.log('click')}>
+          <FaChevronLeft />
+        </button>
+        <button type="button" onClick={() => console.log('click')}>
+          <FaChevronRight />
+        </button>
       </div>
+      <div className={styles.profile}>
+        <div
+          className={`${styles['profile--info']} ${
+            isActive ? styles.active : ''
+          }`}
+          onClick={() => setIsActive(state => !state)}
+        >
+          <div className={styles['profile--info__image']}>
+            <img src="https://avatars.githubusercontent.com/u/11177716?v=4" />
+          </div>
+          <strong>Leonardo Vargas</strong>
 
-      <ul className={styles['profile--menu']}>
-        <li>Conta</li>
-        <li>Pefil</li>
-        <li>Sair</li>
-      </ul>
-    </div>
-  </header>
-)
+          <FaCaretDown />
+        </div>
+
+        {isActive && (
+          <ul className={styles['profile--menu']}>
+            {NAVIGATION.map(navItem => (
+              <li key={navItem.name} className={styles['profile--menu__item']}>
+                <Link href={navItem.pathname}>
+                  <a>
+                    {navItem.name} {navItem.icon}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </header>
+  )
+}
