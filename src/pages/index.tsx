7558aecf7b20fd068a505616055a473b007e0857
Leonardo Vargas <leu1607@hotmail.com>
2022-02-26 18:44:35 -0300

    feat: adicionando os cards na homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,20 @@
 import Head from 'next/head'
+import { FaPlay, FaPause } from 'react-icons/fa'
 import { useEffect, useState } from 'react'
 
+import styles from 'styles/home.module.scss'
+
+const MOCK_CARDS = [
+  'Músicas Curtidas',
+  'Concentration',
+  'Twitch',
+  'Mix de Imagine Dragons',
+  'Mix rock',
+  'Caneca de Mamicas 43 - Tá na hora de lembrar Caneca de Mamicas 43 - Tá na hora de lembrar Caneca de Mamicas 43 - Tá na hora de lembrar Caneca de Mamicas 43 - Tá na hora de lembrar Caneca de Mamicas 43 - Tá na hora de lembrar'
+]
+
 export default function HomePage() {
+  const [activePlay, setActivePlay] = useState<number>(-1)
   const [welcomeMessage, setWelcomeMessage] = useState<string>('')
 
   useEffect(() => {
@@ -12,13 +25,34 @@ export default function HomePage() {
     else setWelcomeMessage('Boa noite')
   }, [])
 
+  const handleActivePlay = (cardIndex: number): void => {
+    setActivePlay(state => (state === cardIndex ? -1 : cardIndex))
+  }
+
   return (
     <>
-      <div>
-        <Head>
-          <title>HomePage</title>
-        </Head>
-        <h1>{welcomeMessage}</h1>
+      <Head>
+        <title>HomePage</title>
+      </Head>
+      <h1>{welcomeMessage}</h1>
+
+      <div className={styles.cards}>
+        {MOCK_CARDS.map((card, index) => (
+          <div
+            key={card}
+            className={`${styles.card} ${
+              activePlay === index ? styles.active : ''
+            }`}
+          >
+            <span />
+            <strong>{card}</strong>
+            <div className={styles['card--button']}>
+              <button type="button" onClick={() => handleActivePlay(index)}>
+                {activePlay === index ? <FaPause /> : <FaPlay />}
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   )
