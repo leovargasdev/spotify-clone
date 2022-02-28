79f517a7ec70032c344d78fdca86285ab27e7463
Leonardo Vargas <leu1607@hotmail.com>
2022-02-28 18:26:12 -0300

    styles: configurando os icones da aplicação

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const withReactSvg = require('next-react-svg')
+const path = require('path')
+
+module.exports = withReactSvg({
   webpack5: true,
-}
+  include: path.resolve(__dirname, 'src/static/'),
+  webpack(config, options) {
+    return config
+  }
+})
---
+++ src/static/index.ts
@@ -0,0 +1,33 @@
+import IconConnectDevice from './icons/connect-device.svg'
+import IconEpisode from './icons/episode.svg'
+import IconExpand from './icons/expand.svg'
+import IconHeart from './icons/heart.svg'
+import IconHome from './icons/home.svg'
+import IconLibrary from './icons/library.svg'
+import IconList from './icons/list.svg'
+import IconLoop from './icons/loop.svg'
+import IconMicrophone from './icons/microphone.svg'
+import IconRepeat from './icons/repeat.svg'
+import IconSearch from './icons/search.svg'
+import IconSkipEnd from './icons/skipEnd.svg'
+import IconSkipStart from './icons/skipStart.svg'
+import IconSound from './icons/sound.svg'
+import IconTray from './icons/tray.svg'
+
+export {
+  IconConnectDevice,
+  IconEpisode,
+  IconExpand,
+  IconHeart,
+  IconHome,
+  IconLibrary,
+  IconList,
+  IconLoop,
+  IconMicrophone,
+  IconRepeat,
+  IconSearch,
+  IconSkipEnd,
+  IconSkipStart,
+  IconSound,
+  IconTray
+}
