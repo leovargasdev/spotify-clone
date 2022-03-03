4d3d0f9773dcba260e7b7e03b812002ce8ec0df5
Leonardo Vargas <leu1607@hotmail.com>
2022-03-03 20:23:38 -0300

    build: atualizando url do host do serviço api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
+const isDev = process.env.VERCEL_ENV === 'develop'
+const url = isDev
+  ? 'http://localhost:3000/'
+  : 'https://spotify.leonardovargas.dev/'
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api/'
+  baseURL: 'api/'
 })
